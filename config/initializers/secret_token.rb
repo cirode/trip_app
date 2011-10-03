eb3ac502dfcd02daf861e667ca15fa40208ab244
Christopher Rode <cirode@Charon.local>
2011-10-03 16:16:04 +1100

    Initial commit for the trip_application Better name to be thought up later

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TripApp::Application.config.secret_token = 'e9628e4328d42544aba468a57aa9bfd1b55f2e4933decbdb932bcbb14d374cae8085b1b12dd92b8a0e94bc1caac64ad255eaca6ce4b69f000d1bab51d2a966cc'
